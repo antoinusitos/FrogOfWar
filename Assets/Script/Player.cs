6ee5478e7f84bf3be456a542bb3a10a62d4aea1f
antoinusitos <a.gargasson@rubika-edu.com>
2015-11-24 18:12:30 +0100

    debut ajout fog

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -20,6 +20,9 @@ public class Player : MonoBehaviour {
     public Vector3 _spawnPoint;
     public int _scoreMax;
 
+    public Transform FogOfWarPlane;
+    public int Number = 1;
+
     // Use this for initialization
     void Start ()
     {
@@ -38,7 +41,20 @@ public class Player : MonoBehaviour {
         _possessObjectif = false;
         _scoreMax = 5;
     }
-	
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        Ray rayToPlayerPos = Camera.main.ScreenPointToRay(screenPos);
+        int layermask = (int)(1 << 8);
+        RaycastHit hit;
+        if (Physics.Raycast(rayToPlayerPos, out hit, 1000, layermask))
+        {
+            FogOfWarPlane.GetComponent<Renderer>().material.SetVector("_Player" + Number.ToString() + "_Pos", hit.point);
+        }
+    }
 
     public GameObject GetCase()
     {
